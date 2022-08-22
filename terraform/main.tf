0d206b8c1ae69bf690c980ed4bbcadcce108e402
Martí Bosch <marti.bosch@protonmail.com>
2022-08-22 15:02:49 +0200

    using sodium

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,10 @@ terraform {
       source  = "integrations/github"
       version = "~> 4.0"
     }
+    sodium = {
+      source  = "killmeplz/sodium"
+      version = ">= 0.0.3"
+    }
   }
 }
 
@@ -23,9 +27,18 @@ resource "github_repository" "repo" {
   gitignore_template = "Terraform"
   license_template   = "gpl-3.0"
 }
+data "github_actions_public_key" "gh_actions_public_key" {
+  repository = github_repository.repo.name
+}
+
+
+data "sodium_encrypted_item" "encrypted_file" {
+    public_key_base64 = data.github_actions_public_key.gh_actions_public_key.key
+    content_base64 = filebase64("vars.tfvars")
+}
 
 resource "github_actions_secret" "tfvars" {
   repository      = github_repository.repo.name
   secret_name     = "tfvars"
-  plaintext_value = filebase64("vars.tfvars")
+  plaintext_value = data.sodium_encrypted_item.encrypted_file.encrypted_value_base64
 }
