43b354787865477b788c171ab08de18d30dd11f8
Martí Bosch <marti.bosch@protonmail.com>
2022-08-22 14:23:39 +0200

    added test

---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  cloud {
+    organization = "exaf-epfl"
+    workspaces {
+      name = "tfc-file-secret-test"
+    }
+  }
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+  }
+}
+
+resource "github_repository" "repo" {
+  name        = "tfc-file-secret-test"
+  description = "Test terraform setup for cookiecutter-django-doge."
+  visibility  = "public"
+
+  allow_merge_commit = true
+  auto_init          = true
+  gitignore_template = "Terraform"
+  license_template   = "gpl-3.0"
+}
+
+resource "github_actions_secret" "tfvars" {
+  repository      = github_repository.repo.name
+  secret_name     = "tfvars"
+  encrypted_value = filebase64("vars.tfvars")
+}
